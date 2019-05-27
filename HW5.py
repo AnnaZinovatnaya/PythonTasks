d235931da51a94b34b2c3881916f34b4b07b1af3
AnnaZ <zinovatnaya@gmail.com>
2019-05-27 20:37:09 +0300

    HW5 Decorators

---
+++ HW5.py
@@ -0,0 +1,54 @@
+import time
+
+
+def timer(func):
+    def wrapper(number):
+        start_time = time.time()
+        res = func(number)
+        print('Time = ' + str(time.time() - start_time))
+        return res
+
+    return wrapper
+
+
+def counted(func):
+    counter = 0
+
+    def wrapper(number):
+        nonlocal counter
+        counter += 1
+        print('Call ' + str(counter))
+
+        return func(number)
+
+    return wrapper
+
+
+def cached(func):
+    history = {}
+
+    def wrapper(number):
+        if number in history.keys():
+            print("Return from history")
+        else:
+            print("Calculate")
+            history[number] = func(number)
+        return history[number]
+
+    return wrapper
+
+
+@timer
+@counted
+@cached
+def my_power_two(number):
+    return number * number
+
+
+print(my_power_two(1))
+print(my_power_two(4))
+print(my_power_two(6))
+print(my_power_two(1))
+print(my_power_two(6))
+print(my_power_two(6))
+
